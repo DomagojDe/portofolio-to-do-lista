201fc3e47ff26cedef04cd801658a5b68f8fcc78
Polaznik <Polaznik>
2016-07-29 08:37:35 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+
+$(".action-add").click(function() {
+       
+
+ $(".main-list").append("<li>" + 
+
+
+$(".item-input").val() + " <button type='button' class='action-delete'> X </button></li>");
+        
+
+$(".item-input").val("");
+    });
+    
+    //Deleting items from list
+    $("ul").on("click", ".action-delete", function(){
+        $(this).parent().remove();
+    });
+});
